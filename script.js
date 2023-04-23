cb32b9acc8e6969968afb5d3597f4f4f55691ba0
Ankita Gupta <120979026+ankita34359@users.noreply.github.com>
2023-04-23 11:58:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+
+// Selecting HTML elements
+
+const todoInput = document.getElementById('todoInput');
+
+const addBtn = document.getElementById('addBtn');
+const todoList = document.getElementById('todoList');
+
+// Adding event listner to the add button
+addBtn.addEventListener('click', addTodo);
+
+// Function to add a new Todo
+function addTodo(){
+
+  // get the value of the todo input
+  const todoText = todoInput.value;
+
+  // create a new list item
+  const listItem = document.createElement('li');
+
+  // Add the todo text to the list item Element
+  listItem.innerText = todoText;
+
+  // Append the list item element to thedo list
+  todoList.appendChild(listItem);
+
+  // clear todo input
+  todoInput.value = '';
+}
